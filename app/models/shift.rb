bc7795b9eb3ff94288603ab604cb690f1e2af902
= <kimgors@gmail.com>
2021-04-02 17:10:40 +0800

    update time

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,2 +1,15 @@
 class Shift < ApplicationRecord
+
+  def as_json(options={})
+    options[:methods] = [:converted_start_time, :converted_end_time]
+    super
+  end
+
+  def converted_start_time
+    start_time.strftime("%H:%M")
+  end
+
+  def converted_end_time
+    end_time.strftime("%H:%M")
+  end
 end
